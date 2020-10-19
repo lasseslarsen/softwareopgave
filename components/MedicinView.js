dccfcb690b37dbd588733ab79ee92002dc911ccc
Lasse <lasse_sl@me.com>
2020-10-19 18:40:42 +0200

    Færdig

--- components/MedicinView.js
+++ components/MedicinView.js
@@ -1,9 +1,9 @@
 import React,{Component} from 'react';
 import {Text, View, ScrollView, StyleSheet, Image, TouchableOpacity} from 'react-native';
-import img from '/Users/Lasse/Desktop/sites/softwareopgave/assets/panodil.jpg';
-import img2 from '/Users/Lasse/Desktop/sites/softwareopgave/assets/panodil-extra.jpg';
-import img3 from '/Users/Lasse/Desktop/sites/softwareopgave/assets/panodil-hot.jpg';
-import img4 from '/Users/Lasse/Desktop/sites/softwareopgave/assets/panodil-zapp.jpg';
+import img from '/Users/Lasse/Desktop/sites/SoftwareOpgave/assets/panodil.jpg';
+import img2 from '/Users/Lasse/Desktop/sites/SoftwareOpgave/assets/panodil-extra.jpg';
+import img3 from '/Users/Lasse/Desktop/sites/SoftwareOpgave/assets/panodil-hot.jpg';
+import img4 from '/Users/Lasse/Desktop/sites/SoftwareOpgave/assets/panodil-zapp.jpg';
 
 export default class ScrollViewScreen extends Component{
 
